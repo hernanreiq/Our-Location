bc4d43c4990eb0562d0898be401377830998011d
Reiq.Laptop <reiqvideos@gmail.com>
2021-09-02 17:44:26 -0400

    Alertas cuando se conecta un nuevo usuario y marcador diferente para usuarios

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,20 @@ var map = L.map('map-template', {
     zoom: 13
 });
 
+var userIcon = L.icon({
+    iconUrl: '/img/user.png',
+    iconSize: [30, 35],
+    iconAnchor: [20, 30],
+    popupAnchor: [-3, -30],
+});
+
+var myuserIcon = L.icon({
+    iconUrl: '/img/myuser.png',
+    iconSize: [30, 35],
+    iconAnchor: [20, 30],
+    popupAnchor: [-3, -30],
+});
+
 const tileURL = 'http://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 
 L.tileLayer(tileURL).addTo(map);
@@ -14,7 +28,7 @@ L.tileLayer(tileURL).addTo(map);
 map.locate({enableHighAccuracy: true});
 map.on('locationfound', e => {
     var coords = [e.latlng.lat, e.latlng.lng];
-    L.marker(coords)
+    L.marker(coords, {icon: myuserIcon})
     .bindPopup('You are here!')
     .addTo(map);
 
@@ -23,11 +37,24 @@ map.on('locationfound', e => {
 });
 
 socket.on('user_connected', (coords) => {
-    L.marker([coords.lat, coords.lng])
+    L.marker([coords.lat, coords.lng], {icon: userIcon})
     .bindPopup('This is an user')
     .addTo(map);
+    bootstrapAlerts('A new user is connected', 'alert-success');
 });
 
 socket.on('users_online', (countUsers) => {
     user_count.innerText = countUsers;
 });
+
+//ENVIAR ALERTAS AL USUARIO
+function bootstrapAlerts(message, typeAlert) {
+    document.getElementById('container_alerts').innerHTML += `
+    <div class="alert ${typeAlert} alert-dismissible fade show" role="alert">
+        <strong>${message}</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    `; 
+}
